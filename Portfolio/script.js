3388f7f6aab55be20aeed1606ed1b4b787f24505
Raphaëll Maïwen <raphaell.maiwen@gmail.com>
2026-04-02 14:59:33 -0400

    Bullet points and video

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -3,15 +3,32 @@ const init = ()=>{
 
     const items = [{
             name: "Compass",
-            description: "An ambitious open-world VR game.",
+            description: `An ambitious open-world VR game.
+            <br>Tasks / Responsibilities:
+            <ul>
+                <li>Blabla</li>
+                <li>Pouet</li>
+                <li>Blabla</li>
+                <li>Pouet</li>
+                <li>Blabla</li>
+                <li>Pouet</li>
+                <li>Blabla</li>
+                <li>Pouet</li>
+                <li>Blabla</li>
+                <li>Pouet</li>
+                <li>Blabla</li>
+                <li>Pouet</li>
+            </ul>`,
             thumbnail: "compassPortfolio.gif",
-            repo: "WetWhatWhat",
-            link: "https://www.meta.com/fr-ca/experiences/compass/7364112793688009/"
+            repo: false,
+            link: "https://www.meta.com/fr-ca/experiences/compass/7364112793688009/",
+            firstLinkText: "Meta Quest Store"
         },
         {
             name: "Circle the Drain",
             description: "Short horror game about the rise and fall of a pop icon.",
-            thumbnail: "circleTheDrainPortfolio.gif",
+            thumbnail: false,
+            videoThumbnail: "thumbnails/circleTheDrainPortfolio.webm",
             repo: "Circle-the-Drain",
             link: "https://rafiki.itch.io/guitar-ego"
         },
@@ -28,13 +45,64 @@ const init = ()=>{
         const root = itemTemplate.cloneNode(true);
         root.querySelector(".name").innerHTML = item.name;
         root.querySelector(".description").innerHTML = item.description;
-        root.querySelector(".thumbnail").src = "thumbnails/" + item.thumbnail;
+        
+        console.log(item.thumbnail);
+        
+        if(!item.thumbnail)
+        {
+            console.log("Video");
+            root.querySelector(".thumbnail").remove();
+            root.querySelector(".videoThumbnail").src = item.videoThumbnail;
+        }
+        else
+        {
+            root.querySelector(".videoThumbnail").remove();
+            root.querySelector(".thumbnail").src = "thumbnails/" + item.thumbnail;
+        }
+        
         root.querySelector(".tryIt").href = item.link;
-        if(item.repo.includes("https://")){
+        
+        if(item.firstLinkText)
+        {
+            root.querySelector(".tryIt").innerHTML = item.firstLinkText;
+        }
+        
+        if(!item.repo)
+        {
+            root.querySelector(".github").remove();
+        }
+        else if(item.repo.includes("https://")){
             root.querySelector(".github").href = item.repo;
         } else {
             root.querySelector(".github").href = "https://github.com/Raphaell-Maiwen/"+item.repo;
         }
-        document.body.appendChild(root);
+        
+        document.querySelector(".selectedProjects").appendChild(root);
     })
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
