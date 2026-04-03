aec9ea2803b95852ef236a30eb086c5417ab7d5e
Raphaëll Maïwen <raphaell.maiwen@gmail.com>
2026-04-03 02:22:11 -0400

    Updated text

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -6,10 +6,10 @@ const init = ()=>{
             description: `An ambitious open-world VR game.
             <br>Tasks / Responsibilities:
             <ul>
-                <li>Gameplay features (elaborate)</li>
+                <li>Gameplay features (including but not limited to: garage to customize ship, platforming obstacles, dynamic dialogue boxes, quest system)</li>
                 <li>Data persistence: making sure the game state stays consistent when saving / loading a save file, and when loading / unloading game zones as the player travel the world.</li>
-                <li>Tooling (give examples)</li>
-                <li>Optimization (LOD Groups, LOD generator, Origin Shifting, other stuff)</li>
+                <li>Tooling (teleport points, color blindness simulator, ship metrics with )</li>
+                <li>Optimization (LOD Groups, LOD generator, origin shifting)</li>
                 <li>UI (UI Toolkit, <a href = "https://discussions.unity.com/t/introducing-the-vector-api-in-unity-2022-1/864911">Vector API)</a></li>
                 <li><a href = "https://www.articy.com/en/">Articy integration</a> (a narrative design tool)</li>
             </ul>`,
@@ -95,29 +95,3 @@ const init = ()=>{
         document.querySelector(".selectedProjects").appendChild(root);
     })
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
